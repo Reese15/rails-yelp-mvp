e84775b3098883dec3f2302236387fa88da28c13
Ghizlane Derdabi <derdabig@gmail.com>
2019-08-07 18:47:39 +0100

    lkhj

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning db ...'
+Restaurant.destroy_all
+
+rest = [
+{
+  name: 'Epicurien',
+  address: 'Marrakech',
+  phone_number: '',
+  category: 'french'
+},
+{
+  name: 'Oncle Blend',
+  address: 'Casablanca',
+  phone_number: '',
+  category: 'french'
+},
+{
+  name: 'Oxymore',
+  address: 'Paris 75011 ',
+  phone_number: '',
+  category: 'italian'
+},
+{
+  name: 'Naga',
+  address: 'Hay ryad, Rabat',
+  phone_number: '',
+  category: 'chinese'
+},
+{
+  name: 'Leon de B',
+  address: 'Paris 75003',
+  phone_number: '06 13 12 12 12',
+  category: 'belgian'
+}
+]
+
+puts 'Creating restaurants'
+Restaurant.create!(rest)
+
+puts 'seed complete'
+
